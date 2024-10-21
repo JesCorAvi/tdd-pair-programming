c0fc06b67281014dd9c60bba7d5d79220dacc0fd
JesCorAvi <jesuscorderoavila1@gmail.com>
2024-10-21 11:34:48 +0200

    test de ejericico 318

---
+++ test_318.py
@@ -0,0 +1,10 @@
+from ejercicio_318 import es_palindromo  
+
+def test_es_palindromo():
+    assert es_palindromo("radar") == True
+    assert es_palindromo("reconocer") == True
+    assert es_palindromo("ana") == True  
+    
+    assert es_palindromo("palabra") == False
+    assert es_palindromo("python") == False
+    assert es_palindromo("programa") == False
