d6d32904c79b6b153ae05e7d9c727d465cd02fd4
Álvaro González <gondialvaro@gmail.com>
2024-10-21 11:31:17 +0200

    Create test_319.py
    Test del ejercicio 3.1.9

---
+++ test_319.py
@@ -0,0 +1,17 @@
+
+palabra = 'plátano'
+
+def test_num_a():
+    assert num_a(palabra) == 2
+
+def test_num_e():
+    assert num_e(palabra) == 0
+
+def test_num_i():
+    assert num_i(palabra) == 0
+
+def test_num_o():
+    assert num_o(palabra) == 1
+
+def test_num_u():
+    assert num_u(palabra) == 0
