fe47b173a36a4ba4ee253c5c9e5ad5ab196e8e96
Álvaro González <gondialvaro@gmail.com>
2024-10-21 11:57:16 +0200

    Ejercicio_318
    Añadido el ejercicio 3.1.8 el cual pasa el test

--- ejercicio_318.py
+++ ejercicio_318.py
@@ -1,3 +1,12 @@
-input('Dame una palabra y te diré si es un palíndromo o no:')
-
 def es_palindromo(palabra: str):
+    if palabra == palabra[::-1]:
+        print('La palabra', palabra, 'es un palíndromo')
+        return True
+    else:
+        print('La palabra', palabra, 'no es un palíndromo')
+        return False
+    
+
+if __name__ == "__main__":
+    palabra_usu = input('Dame una palabra y te diré si es un palíndromo o no: ')
+    es_palindromo(palabra_usu)
